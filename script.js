af5e510391b680292a449afca26cc0c024ab207f
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2019-12-23 06:19:29 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,393 @@
+let notaE1 = document.getElementById('nE1')
+let notaE2 = document.getElementById('nE2')
+let notaE3 = document.getElementById('nE3')
+let notaE4 = document.getElementById('nE4')
+let notaE5 = document.getElementById('nE5')
+let notaE6 = document.getElementById('nE6')
+let notaE7 = document.getElementById('nE7')
+let notaE8 = document.getElementById('nE8')
+let notaE9 = document.getElementById('nE9')
+let notaE10 = document.getElementById('nE10')
+let notaE11 = document.getElementById('nE11')
+let notaE12 = document.getElementById('nE12')
+let notaE13 = document.getElementById('nE13')
+let notaE14 = document.getElementById('nE14')
+let notaE15 = document.getElementById('nE15')
+let notaE16 = document.getElementById('nE16')
+
+let notaB1 = document.getElementById('nB1')
+let notaB2 = document.getElementById('nB2')
+let notaB3 = document.getElementById('nB3')
+let notaB4 = document.getElementById('nB4')
+let notaB5 = document.getElementById('nB5')
+let notaB6 = document.getElementById('nB6')
+let notaB7 = document.getElementById('nB7')
+let notaB8 = document.getElementById('nB8')
+let notaB9 = document.getElementById('nB9')
+let notaB10 = document.getElementById('nB10')
+let notaB11 = document.getElementById('nB11')
+let notaB12 = document.getElementById('nB12')
+let notaB13 = document.getElementById('nB13')
+let notaB14 = document.getElementById('nB14')
+
+
+let notaA1 = document.getElementById('nA1')
+let notaA2 = document.getElementById('nA2')
+let notaA3 = document.getElementById('nA3')
+let notaA4 = document.getElementById('nA4')
+let notaA5 = document.getElementById('nA5')
+let notaA6 = document.getElementById('nA6')
+let notaA7 = document.getElementById('nA7')
+let notaA8 = document.getElementById('nA8')
+let notaA9 = document.getElementById('nA9')
+let notaA10 = document.getElementById('nA10')
+let notaA11 = document.getElementById('nA11')
+let notaA12 = document.getElementById('nA12')
+let notaA13 = document.getElementById('nA13')
+let notaA14 = document.getElementById('nA14')
+let notaA15 = document.getElementById('nA15')
+
+
+let botaoE = document.getElementById('e7')
+let botaoA = document.getElementById('a7')
+let botaoB = document.getElementById('b7')
+
+
+
+botaoE.onmouseout = function () {
+
+    notaE1.style.backgroundColor = 'gray'
+    notaE2.style.backgroundColor = 'gray'
+    notaE3.style.backgroundColor = 'gray'
+    notaE4.style.backgroundColor = 'gray'
+    notaE5.style.backgroundColor = 'gray'
+    notaE6.style.backgroundColor = 'gray'
+    notaE7.style.backgroundColor = 'gray'
+    notaE8.style.backgroundColor = 'gray'
+    notaE9.style.backgroundColor = 'gray'
+    notaE10.style.backgroundColor = 'gray'
+    notaE11.style.backgroundColor = 'gray'
+    notaE12.style.backgroundColor = 'gray'
+    notaE13.style.backgroundColor = 'gray'
+    notaE14.style.backgroundColor = 'gray'
+    notaE15.style.backgroundColor = 'gray'
+    notaE16.style.backgroundColor = 'gray'
+
+    notaA1.style.backgroundColor = 'gray'
+    notaA2.style.backgroundColor = 'gray'
+    notaA3.style.backgroundColor = 'gray'
+    notaA4.style.backgroundColor = 'gray'
+    notaA5.style.backgroundColor = 'gray'
+    notaA6.style.backgroundColor = 'gray'
+    notaA7.style.backgroundColor = 'gray'
+    notaA8.style.backgroundColor = 'gray'
+    notaA9.style.backgroundColor = 'gray'
+    notaA10.style.backgroundColor = 'gray'
+    notaA11.style.backgroundColor = 'gray'
+    notaA12.style.backgroundColor = 'gray'
+    notaA13.style.backgroundColor = 'gray'
+    notaA14.style.backgroundColor = 'gray'
+    notaA15.style.backgroundColor = 'gray'    
+}
+
+botaoB.onmouseout = function () {
+
+    
+
+    notaB1.style.backgroundColor = 'gray'
+    notaB2.style.backgroundColor = 'gray'
+    notaB3.style.backgroundColor = 'gray'
+    notaB4.style.backgroundColor = 'gray'
+    notaB5.style.backgroundColor = 'gray'
+    notaB6.style.backgroundColor = 'gray'
+    notaB7.style.backgroundColor = 'gray'
+    notaB8.style.backgroundColor = 'gray'
+    notaB9.style.backgroundColor = 'gray'
+    notaB10.style.backgroundColor = 'gray'
+    notaB11.style.backgroundColor = 'gray'
+    notaB12.style.backgroundColor = 'gray'
+    notaB13.style.backgroundColor = 'gray'
+    notaB14.style.backgroundColor = 'gray'
+
+    notaE1.style.backgroundColor = 'gray'
+    notaE2.style.backgroundColor = 'gray'
+    notaE3.style.backgroundColor = 'gray'
+    notaE4.style.backgroundColor = 'gray'
+    notaE5.style.backgroundColor = 'gray'
+    notaE6.style.backgroundColor = 'gray'
+    notaE7.style.backgroundColor = 'gray'
+    notaE8.style.backgroundColor = 'gray'
+    notaE9.style.backgroundColor = 'gray'
+    notaE10.style.backgroundColor = 'gray'
+    notaE11.style.backgroundColor = 'gray'
+    notaE12.style.backgroundColor = 'gray'
+    notaE13.style.backgroundColor = 'gray'
+    notaE14.style.backgroundColor = 'gray'
+    notaE15.style.backgroundColor = 'gray'
+    notaE16.style.backgroundColor = 'gray'
+    
+    
+
+    notaA1.style.backgroundColor = 'gray'
+    notaA2.style.backgroundColor = 'gray'
+    notaA3.style.backgroundColor = 'gray'
+    notaA4.style.backgroundColor = 'gray'
+    notaA5.style.backgroundColor = 'gray'
+    notaA6.style.backgroundColor = 'gray'
+    notaA7.style.backgroundColor = 'gray'
+    notaA8.style.backgroundColor = 'gray'
+    notaA9.style.backgroundColor = 'gray'
+    notaA10.style.backgroundColor = 'gray'
+    notaA11.style.backgroundColor = 'gray'
+    notaA12.style.backgroundColor = 'gray'
+    notaA13.style.backgroundColor = 'gray'
+    notaA14.style.backgroundColor = 'gray'
+    notaA15.style.backgroundColor = 'gray'    
+}
+
+botaoA.onmouseout = function () {
+
+    notaE1.style.backgroundColor = 'gray'
+    notaE2.style.backgroundColor = 'gray'
+    notaE3.style.backgroundColor = 'gray'
+    notaE4.style.backgroundColor = 'gray'
+    notaE5.style.backgroundColor = 'gray'
+    notaE6.style.backgroundColor = 'gray'
+    notaE7.style.backgroundColor = 'gray'
+    notaE8.style.backgroundColor = 'gray'
+    notaE9.style.backgroundColor = 'gray'
+    notaE10.style.backgroundColor = 'gray'
+    notaE11.style.backgroundColor = 'gray'
+    notaE12.style.backgroundColor = 'gray'
+    notaE13.style.backgroundColor = 'gray'
+    notaE14.style.backgroundColor = 'gray'
+    notaE15.style.backgroundColor = 'gray'
+    notaE16.style.backgroundColor = 'gray'
+
+    notaA1.style.backgroundColor = 'gray'
+    notaA2.style.backgroundColor = 'gray'
+    notaA3.style.backgroundColor = 'gray'
+    notaA4.style.backgroundColor = 'gray'
+    notaA5.style.backgroundColor = 'gray'
+    notaA6.style.backgroundColor = 'gray'
+    notaA7.style.backgroundColor = 'gray'
+    notaA8.style.backgroundColor = 'gray'
+    notaA9.style.backgroundColor = 'gray'
+    notaA10.style.backgroundColor = 'gray'
+    notaA11.style.backgroundColor = 'gray'
+    notaA12.style.backgroundColor = 'gray'
+    notaA13.style.backgroundColor = 'gray'
+    notaA14.style.backgroundColor = 'gray'
+    notaA15.style.backgroundColor = 'gray'
+
+
+    
+}
+
+
+
+botaoE.onmouseover = function () {
+
+    notaB1.style.display = 'none'
+    notaB2.style.display = 'none'
+    notaB3.style.display = 'none'
+    notaB4.style.display = 'none'
+    notaB5.style.display = 'none'
+    notaB6.style.display = 'none'
+    notaB7.style.display = 'none'
+    notaB8.style.display = 'none'
+    notaB9.style.display = 'none'
+    notaB10.style.display = 'none'
+    notaB11.style.display = 'none'
+    notaB12.style.display = 'none'
+    notaB13.style.display = 'none'
+    notaB14.style.display = 'none'
+
+    notaE1.style.display = 'block'
+    notaE2.style.display = 'block'
+    notaE3.style.display = 'block'
+    notaE4.style.display = 'block'
+    notaE5.style.display = 'block'
+    notaE6.style.display = 'block'
+    notaE7.style.display = 'block'
+    notaE8.style.display = 'block'
+    notaE9.style.display = 'block'
+    notaE10.style.display = 'block'
+    notaE11.style.display = 'block'
+    notaE12.style.display = 'block'
+    notaE13.style.display = 'block'
+    notaE14.style.display = 'block'
+    notaE15.style.display = 'block'
+    notaE16.style.display = 'block'
+
+    notaE1.style.backgroundColor = 'greenyellow'
+    notaE2.style.backgroundColor = 'orange'
+    notaE3.style.backgroundColor = 'blue'
+    notaE4.style.backgroundColor = 'greenyellow'
+    notaE5.style.backgroundColor = 'greenyellow'
+    notaE6.style.backgroundColor = 'orange'
+    notaE7.style.backgroundColor = 'greenyellow'
+    notaE8.style.backgroundColor = 'orange'
+    notaE9.style.backgroundColor = 'blue'
+    notaE10.style.backgroundColor = 'greenyellow'
+    notaE11.style.backgroundColor = 'greenyellow'
+    notaE12.style.backgroundColor = 'orange'
+    notaE13.style.backgroundColor = 'greenyellow'
+    notaE14.style.backgroundColor = 'orange'
+    notaE15.style.backgroundColor = 'blue'
+    notaE16.style.backgroundColor = 'greenyellow'
+
+    notaA1.style.display = 'none'
+    notaA2.style.display = 'none'
+    notaA3.style.display = 'none'
+    notaA4.style.display = 'none'
+    notaA5.style.display = 'none'
+    notaA6.style.display = 'none'
+    notaA7.style.display = 'none'
+    notaA8.style.display = 'none'
+    notaA9.style.display = 'none'
+    notaA10.style.display = 'none'
+    notaA11.style.display = 'none'
+    notaA12.style.display = 'none'
+    notaA13.style.display = 'none'
+    notaA14.style.display = 'none'
+    notaA15.style.display = 'none'
+    
+    
+}
+
+botaoA.onmouseover = function () {
+
+    notaB1.style.display = 'none'
+    notaB2.style.display = 'none'
+    notaB3.style.display = 'none'
+    notaB4.style.display = 'none'
+    notaB5.style.display = 'none'
+    notaB6.style.display = 'none'
+    notaB7.style.display = 'none'
+    notaB8.style.display = 'none'
+    notaB9.style.display = 'none'
+    notaB10.style.display = 'none'
+    notaB11.style.display = 'none'
+    notaB12.style.display = 'none'
+    notaB13.style.display = 'none'
+    notaB14.style.display = 'none'
+    
+
+    notaA1.style.display = 'block'
+    notaA2.style.display = 'block'
+    notaA3.style.display = 'block'
+    notaA4.style.display = 'block'
+    notaA5.style.display = 'block'
+    notaA6.style.display = 'block'
+    notaA7.style.display = 'block'
+    notaA8.style.display = 'block'
+    notaA9.style.display = 'block'
+    notaA10.style.display = 'block'
+    notaA11.style.display = 'block'
+    notaA12.style.display = 'block'
+    notaA13.style.display = 'block'
+    notaA14.style.display = 'block'
+    notaA15.style.display = 'block'
+
+    notaE1.style.display = 'none'
+    notaE2.style.display = 'none'
+    notaE3.style.display = 'none'
+    notaE4.style.display = 'none'
+    notaE5.style.display = 'none'
+    notaE6.style.display = 'none'
+    notaE7.style.display = 'none'
+    notaE8.style.display = 'none'
+    notaE9.style.display = 'none'
+    notaE10.style.display = 'none'
+    notaE11.style.display = 'none'
+    notaE12.style.display = 'none'
+    notaE13.style.display = 'none'
+    notaE14.style.display = 'none'
+    notaE15.style.display = 'none'
+    notaE16.style.display = 'none'
+
+    notaA1.style.backgroundColor = 'greenyellow'
+    notaA2.style.backgroundColor = 'orange'
+    notaA3.style.backgroundColor = 'greenyellow'
+    notaA4.style.backgroundColor = 'orange'
+    notaA5.style.backgroundColor = 'blue'
+    notaA6.style.backgroundColor = 'greenyellow'
+    notaA7.style.backgroundColor = 'greenyellow'
+    notaA8.style.backgroundColor = 'orange'
+    notaA9.style.backgroundColor = 'greenyellow'
+    notaA10.style.backgroundColor = 'orange'
+    notaA11.style.backgroundColor = 'blue'
+    notaA12.style.backgroundColor = 'greenyellow'
+    notaA13.style.backgroundColor = 'greenyellow'
+    notaA14.style.backgroundColor = 'orange'
+    notaA15.style.backgroundColor = 'greenyellow'
+}
+
+botaoB.onmouseover = function () {
+
+    notaB1.style.backgroundColor = "greenyellow"
+    notaB2.style.backgroundColor = "orange"
+    notaB3.style.backgroundColor = "greenyellow"
+    notaB4.style.backgroundColor = "orange"
+    notaB5.style.backgroundColor = "blue"
+    notaB6.style.backgroundColor = "greenyellow"
+    notaB7.style.backgroundColor = "greenyellow"
+    notaB8.style.backgroundColor = "orange"
+    notaB9.style.backgroundColor = "greenyellow"
+    notaB10.style.backgroundColor = "orange"
+    notaB11.style.backgroundColor = "blue"
+    notaB12.style.backgroundColor = "greenyellow"
+    notaB13.style.backgroundColor = "greenyellow"
+    notaB14.style.backgroundColor = "orange"
+ 
+    notaB1.style.display = 'block'
+    notaB2.style.display = 'block'
+    notaB3.style.display = 'block'
+    notaB4.style.display = 'block'
+    notaB5.style.display = 'block'
+    notaB6.style.display = 'block'
+    notaB7.style.display = 'block'
+    notaB8.style.display = 'block'
+    notaB9.style.display = 'block'
+    notaB10.style.display = 'block'
+    notaB11.style.display = 'block'
+    notaB12.style.display = 'block'
+    notaB13.style.display = 'block'
+    notaB14.style.display = 'block'
+    
+
+    notaE1.style.display = 'none'
+    notaE2.style.display = 'none'
+    notaE3.style.display = 'none'
+    notaE4.style.display = 'none'
+    notaE5.style.display = 'none'
+    notaE6.style.display = 'none'
+    notaE7.style.display = 'none'
+    notaE8.style.display = 'none'
+    notaE9.style.display = 'none'
+    notaE10.style.display = 'none'
+    notaE11.style.display = 'none'
+    notaE12.style.display = 'none'
+    notaE13.style.display = 'none'
+    notaE14.style.display = 'none'
+    notaE15.style.display = 'none'
+    notaE16.style.display = 'none'
+
+    notaA1.style.display = 'none'
+    notaA2.style.display = 'none'
+    notaA3.style.display = 'none'
+    notaA4.style.display = 'none'
+    notaA5.style.display = 'none'
+    notaA6.style.display = 'none'
+    notaA7.style.display = 'none'
+    notaA8.style.display = 'none'
+    notaA9.style.display = 'none'
+    notaA10.style.display = 'none'
+    notaA11.style.display = 'none'
+    notaA12.style.display = 'none'
+    notaA13.style.display = 'none'
+    notaA14.style.display = 'none'
+    notaA15.style.display = 'none'
+}
+
